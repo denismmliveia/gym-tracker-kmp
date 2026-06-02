8b820dbc48908fd5c1701bc436f203dd319eae58
Denis <denis@rave.local>
2026-06-02 13:15:33 +0200

    chore: Gradle wrapper 8.9 and version catalog

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+rootProject.name = "GymTracker"
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
+include(":composeApp")
